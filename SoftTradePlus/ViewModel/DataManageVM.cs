28800dc589015e40b9ebe927e97897b62e7f550f
Shemyako <52855609+Shemyako@users.noreply.github.com>
2022-08-01 13:22:56 +0300

    object editing fixed

--- SoftTradePlus/ViewModel/DataManageVM.cs
+++ SoftTradePlus/ViewModel/DataManageVM.cs
@@ -12,10 +12,13 @@ namespace SoftTradePlus.VievModel
     public class DataManageVM
     {
         ApplicationContext db = new ApplicationContext();
+        // Lists to be displayed for user
         public List<Client> Clients { get; set; }
         public List<Manager> Managers { get; set; }
         public List<Product> Products { get; set; }
         public List<ClientStatus> Statuses { get; set; }
+
+        // Commands
         RelayCommand? addManager;
         RelayCommand? editManager;
         RelayCommand? deleteManager;
@@ -28,8 +31,10 @@ namespace SoftTradePlus.VievModel
         RelayCommand? sellProduct;
         RelayCommand? clietnsProducts;
         RelayCommand? managersClients;
+
         public DataManageVM()
         {
+            // Load lists
             Clients = db.Clients.ToList();
             Managers = db.Managers.ToList();
             Products = db.Products.ToList();
@@ -38,6 +43,28 @@ namespace SoftTradePlus.VievModel
 
         #region METHODS TO WORK WITH MANAGER
 
+        private Manager? Check_Manager(Manager manager)
+        {
+            ManagerWindow managerWindow = new ManagerWindow(manager);
+            bool? result = managerWindow.ShowDialog();
+
+            while (result == true)
+            {
+                if (managerWindow.Manager.Name is not null &&
+                    managerWindow.Manager.Name.Replace(" ", "").Length != 0)
+                    break;
+
+                managerWindow = new ManagerWindow(managerWindow.Manager);
+                managerWindow.textBox.BorderBrush = Brushes.Red;
+
+                result = managerWindow.ShowDialog();
+
+            }
+            if (result == true)
+                return managerWindow.Manager;
+            return null;
+        }
+
         public RelayCommand AddManager
         {
             get
@@ -45,26 +72,10 @@ namespace SoftTradePlus.VievModel
                 return addManager ??
                     (addManager = new RelayCommand((o) =>
                     {
-                        ManagerWindow managerWindow = new ManagerWindow(new Manager());
-                        managerWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-                        bool? result = managerWindow.ShowDialog();
+                        Manager? manager = Check_Manager(new Manager());
 
-                        while (result == true)
+                    if (manager is not null)
                         {
-                            if (managerWindow.Manager.Name is not null &&
-                                managerWindow.Manager.Name.Replace(" ", "").Length != 0)
-                                break;
-                            managerWindow = new ManagerWindow(managerWindow.Manager);
-                            managerWindow.textBox.BorderBrush = Brushes.Red;
-                            managerWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-
-                            result = managerWindow.ShowDialog();
-
-                        }
-
-                        if (result == true)
-                        {
-                            Manager manager = managerWindow.Manager;
                             db.Managers.Add(manager);
                             db.SaveChanges();
 
@@ -87,28 +98,20 @@ namespace SoftTradePlus.VievModel
                         Manager? manager = (Manager?)selectedItem;
                         if (manager is null)
                             return;
-
-                        Manager mg = db.Managers.FirstOrDefault(d => d.Id == manager.Id);
-                        ManagerWindow managerWindow = new ManagerWindow(mg);
-                        managerWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-
-                        bool? result = managerWindow.ShowDialog();
-                        
-                        while (result == true)
+                        manager = new Manager
                         {
-                            if (managerWindow.Manager.Name is not null &&
-                                managerWindow.Manager.Name.Replace(" ", "").Length != 0)
-                                break;
-                            managerWindow = new ManagerWindow(managerWindow.Manager);
-                            managerWindow.textBox.BorderBrush = Brushes.Red;
-                            managerWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+                            Name = manager.Name,
+                            Id = manager.Id
+                        };
 
-                            result = managerWindow.ShowDialog();
-                        }
+                        Manager? mg = db.Managers.FirstOrDefault(d => d.Id == manager.Id);
+                        if (mg is null) return;
 
-                        if (result == true)
+                        manager = Check_Manager(manager);
+
+                        if (manager is not null)
                         {
-                            mg.Name = managerWindow.Manager.Name;
+                            mg.Name = manager.Name;
                             
                             db.SaveChanges();
                             //MainWindow.viewManager.Items.Refresh();
@@ -209,17 +212,29 @@ namespace SoftTradePlus.VievModel
                         Product? product = (Product?)selectedItem;
                         if (product is null)
                             return;
+                        product = new Product
+                        {
+                            Price = product.Price,
+                            Id = product.Id,
+                            Is_sub = product.Is_sub,
+                            Sub_end = product.Sub_end,
+                            Name = product.Name
+                        };
 
                         Product? pd = db.Products.FirstOrDefault(d => d.Id == product.Id);
                         if (pd is null) return;
 
-                        product = Check_Product(pd);
+                        product = Check_Product(product);
 
                         if (product is not null)
                         {
                             if (product.Is_sub == false)
                                 product.Sub_end = null;
 
+                            pd.Price = product.Price;
+                            pd.Id = product.Id;
+                            pd.Is_sub = product.Is_sub;
+                            pd.Sub_end = product.Sub_end;
                             pd.Name = product.Name;
 
                             db.SaveChanges();
@@ -321,21 +336,30 @@ namespace SoftTradePlus.VievModel
                         Client? client = (Client?)selectedItem;
                         if (client is null)
                             return;
+                        client = new Client
+                        {
+                            Name = client.Name,
+                            ClientStatus = client.ClientStatus,
+                            Id = client.Id,
+                            Manager = client.Manager
+                        };
 
                         Client? cl = db.Clients.FirstOrDefault(d => d.Id == client.Id);
                         if (cl is null) return;
 
-                        client = Check_Client(cl);
+                        client = Check_Client(client);
 
                         if (client is not null)
                         {
                             cl.Name = client.Name;
+                            cl.ClientStatus = client.ClientStatus;
+                            cl.Manager = client.Manager;
 
                             db.SaveChanges();
 
-                            Clients = db.Clients.ToList();
+                            //Clients = db.Clients.ToList();
 
-                            MainWindow.viewClient.ItemsSource = Clients;
+                            //MainWindow.viewClient.ItemsSource = Clients;
                         }
                     }));
             }
